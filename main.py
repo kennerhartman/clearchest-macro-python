b9851e77fa692685dec53a3af1f8d2fd3b078e24
Kenner <kennertobiashartman@gmail.com>
2023-05-18 20:43:53 -0600

    This script can only run when the Minecraft window is in focus (active)

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 # import modules
-
 import keyboard
 import mouse
 import time
-    
+import pygetwindow as pg
+
+def checkIsRunning():
+    global flag
+
+    if (pg.getActiveWindowTitle() == "Minecraft* (version hidden from driver)" or pg.getActiveWindowTitle() == "Minecraft* (version hidden from driver) - Singleplayer"):
+        flag = 1
+        # print("Window is active")
+    else:
+        flag = 0
+        # print("Window is not active")
+        
+
 # emulates shift clicking inventory slots
 def shiftClick():
     keyboard.press('left shift')
@@ -16,20 +27,31 @@ def clearChest(x, y):
         shiftClick()
         mouse.move(x, y)
         x += 72
-        time.sleep(25/1000) # DO NOT DECREASE VALUE UNLESS YOU WANT TO BREAK YOUR CURSOR!!!
+        time.sleep(25/1000) # DO NOT DECREASE VALUE UNLESS YOU WANT TO BREAK YOUR CURSOR AND HAVE YOUR LIFE RUINED!!!
     
-while True:
-    if keyboard.is_pressed('left ctrl') and keyboard.is_pressed('b'): 
+def runScript():
+    if keyboard.is_pressed('left shift') and keyboard.is_pressed('b'): 
         x, y = mouse.get_position()
 
         for i in range(0, 3):
             clearChest(x, y)
             y += 70
 
-    if keyboard.is_pressed('left ctrl') and keyboard.is_pressed('`'): 
+    if keyboard.is_pressed('left shift') and keyboard.is_pressed('`'): 
         x, y = mouse.get_position()
 
         clearChest(x, y)
 
+while True:
+    checkIsRunning()
+    time.sleep(1/1000)
+
     if keyboard.is_pressed('left shift') and keyboard.is_pressed('esc'):
         break
+
+    while(flag == 1): 
+        if keyboard.is_pressed('left shift') and keyboard.is_pressed('esc'):
+            break
+
+        checkIsRunning()
+        runScript()
